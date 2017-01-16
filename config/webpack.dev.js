422b35c72131e9b30799b109cfcc377a19a90e83
rjansen <raphael.jansen@e-pedion.com>
2017-01-16 02:52:00 -0200

    Change fivecolors proxy port

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -157,7 +157,7 @@ module.exports = function (options) {
         poll: 1000
       },
       proxy: {
-        '/api/*': 'http://localhost:4000'
+        '/api/*': 'http://localhost:5000'
       },
       output: {
         path: helpers.root('dist'),
